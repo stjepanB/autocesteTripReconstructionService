2db785bede1bc848b7d71a45dfd1cb43be160a39
stjepanB <stjepan.bencic95@gmail.com>
2021-01-18 10:42:04 +0100

    init repo

---
+++ src/main/java/autoceste/trip/reconstruction/ReconstructionApplication.java
@@ -0,0 +1,13 @@
+package autoceste.trip.reconstruction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconstructionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconstructionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/autoceste/trip/reconstruction/ReconstructionApplicationTests.java
@@ -0,0 +1,13 @@
+package autoceste.trip.reconstruction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconstructionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
