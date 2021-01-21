96011d8ff65fe886401400396013a1c33d419c6a
stjepanB <stjepan.bencic95@gmail.com>
2021-01-21 19:02:25 +0100

    some improvement happen

--- src/main/java/autoceste/trip/reconstruction/services/DefaultRecordServiceImpl.java
+++ src/main/java/autoceste/trip/reconstruction/services/DefaultRecordServiceImpl.java
@@ -27,7 +27,7 @@ public class DefaultRecordServiceImpl implements DefaultRecordService {
     private final Map<String, List<DefaultRecord>> activeRecords;
     @Value("${backend.url}")
     private String BACKEND_URL = "http://localhost:9000/api/service";
-    private List<HighwaySection> sections;
+    private List<List<HighwaySection>> sections;
 
     @Autowired
     public DefaultRecordServiceImpl(DefaultRecordRepository recordRepository, SequenceGeneratorService sequenceGeneratorService) {
@@ -40,16 +40,45 @@ public class DefaultRecordServiceImpl implements DefaultRecordService {
                         BACKEND_URL + "/sections",
                         HighwaySectionDto[].class);
 
-        this.sections = Arrays.stream(response.getBody())
+        List<String> sections = Arrays.stream(Objects.requireNonNull(response.getBody()))
                 .map(HighwaySectionDto::toHighwaySections)
+                .map(HighwaySection::getSectionStart)
                 .collect(Collectors.toList());
+
+        List<List<String>> roads = new ArrayList<>();
+
+        roads.add(new ArrayList<>());
+        boolean RIJEKA_DIRECTION = true;
+
+        for (String s : sections) {
+
+            if (RIJEKA_DIRECTION) {
+                roads.get(0).add(s);
+            } else {
+                roads.get(1).add(s);
+            }
+
+            if (s.equals("Bosiljevo")) {
+                roads.add(new ArrayList<>(roads.get(0)));
+            }
+            if (s.equals("Rijeka")) {
+                RIJEKA_DIRECTION = false;
+            }
+        }
+
+        roads.forEach(e -> e.forEach(System.out::println));
 
     }
 
     @Override
     public boolean addRecord(DefaultRecord record) {
         record.setId(sequenceGeneratorService.generateSequence(DefaultRecord.SEQUENCE_NAME));
-        activeRecords.get(record.getPlateMark()).add(record);
+        if (activeRecords.containsKey(record.getPlateMark())) {
+            activeRecords.get(record.getPlateMark()).add(record);
+        } else {
+            activeRecords.put(record.getPlateMark(), new ArrayList<>());
+            activeRecords.get(record.getPlateMark()).add(record);
+        }
         return recordRepository.save(record).getId() != null;
     }
 
@@ -68,18 +97,21 @@ public class DefaultRecordServiceImpl implements DefaultRecordService {
         for (String key : keys) {
             List<DefaultRecord> records = activeRecords.get(key);
 
-            LocalDateTime recent = findNewstRecord(records);
+            LocalDateTime recent = findNewestRecord(records);
 
             if (recent.isBefore(LocalDateTime.now().minusHours(12L))) {
                 trips.add(createTrip(records));
             }
+        }
+        if (trips.isEmpty()) {
+            return;
         }
         saveTrips(trips);
         removeReconstructedTrips(trips);
 
     }
 
-    private LocalDateTime findNewstRecord(List<DefaultRecord> records) {
+    private LocalDateTime findNewestRecord(List<DefaultRecord> records) {
         LocalDateTime recent = records.get(0).getRecordedTime();
 
         for (DefaultRecord r : records) {
@@ -92,14 +124,19 @@ public class DefaultRecordServiceImpl implements DefaultRecordService {
 
     private Trip createTrip(List<DefaultRecord> records) {
         Collections.sort(records);
+
+        for (DefaultRecord r : records) {
+            int i = 0;
+
+        }
         return new Trip(records.get(0), records.get(records.size() - 1));
     }
 
     private boolean saveTrips(List<Trip> trips) {
         RestTemplate restTemplate = new RestTemplate();
 
-        ResponseEntity response = restTemplate.postForObject(
-                BACKEND_URL + "/bill", trips, ResponseEntity.class);
+        var response = restTemplate.postForObject(
+                BACKEND_URL + "/billing", trips, ResponseEntity.class);
 
         return response != null && response.getStatusCode() == HttpStatus.OK;
     }
